dbb936264090d700a8468cad1a6d230ce47f5b7f
Mirfozil <mirfoziln@gmail.com>
2023-03-01 08:19:36 +0500

    add routes

---
+++ client/src/screens/HomeScreen.js
@@ -0,0 +1,36 @@
+import data from '../data.js';
+
+const HomeScreen = {
+   
+    render: () => {
+        const {products} = data;
+        return `
+            <ul class="products">
+                ${products.map(product => `
+                    <li>
+                    <div class="product">
+                    <a href="/#/product/${product.id}">
+                        <img src="${product.image}" alt="${product.name}">
+                    </a>
+                    <div class="product-name">
+                        <a href="/#/product/1">
+                            ${product.name}
+                        </a>
+                    </div>
+                    <div class="product-brand">
+                        ${product.brand}
+                    </div>
+                    <div class="product-price">
+                        <span>
+                            $${product.price}
+                        </span>
+                    </div>
+                </div>
+                    </li>
+                `).join('\n')}
+            </ul>
+        `
+    }
+}
+
+export default HomeScreen;
