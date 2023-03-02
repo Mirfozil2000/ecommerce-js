4f2868db0c2a692f5f89b664e6f209a647d8d8f6
Mirfozil <mirfoziln@gmail.com>
2023-03-02 12:27:16 +0500

    refresh js to react

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import './style/App.css';
+
+function App() {
+
+  return (
+
+    <div className="grid-container">
+    <header>
+      <div className="left__content">
+        <a href="/#/" className='brand'>Ecommerce</a>
+      </div>
+      <div className="right__content">
+        <a href="/#/signin">Sign In</a>
+        <a href="/#/card">Cart</a>
+      </div>
+    </header>
+    </div>
+     
+  )
+}
+
+export default App
+
+
+{/* <main className="main-container"></main>
+    <footer>
+      All rights reserver @2023
+    </footer> */}
---
+++ client/src/utils.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const parseRequestUrl = () => {
+    const url = document.location.hash.toLowerCase();
+    const request = url.split('/');
+    return (
+        {
+            resource: request[1],
+            id: request[2],
+            action: request[3],
+        }
+    )
+}
+
+export default parseRequestUrl
